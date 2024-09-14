a6c0312b91ea3a0aaed865ae4c5e1b6c2e273611
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2024-09-14 08:36:11 +0700

    fixup

--- jay-site/src/components/screenCard/fullStack.tsx
+++ jay-site/src/components/screenCard/fullStack.tsx
@@ -2,7 +2,26 @@ function FullStack(){
     return(
         <div className="screenCard3">
             <div>
-                <img src="../../assets/ace3.jpg" alt="" />
+                <div className="full">フルスタック開発</div>
+                <div className="wow"></div>
+                <h2>“from front end to back end,from designing to development<br></br>
+                full stack-full baked. its just a piece of cake for me!* ”</h2>
+                <h5>*text may be greatly exaggerated  </h5>
+                <br />
+                <h1>software and tools</h1>
+                <br />
+                <div>
+                    <img src="./src/assets/logo/html.png" alt="" />
+                    <img src="./src/assets/logo/css.png" alt="" />
+                    <img src="./src/assets/logo/js.png" alt="" />
+                    <img src="./src/assets/logo/bootstrap.png" alt="" />
+                    <img src="./src/assets/logo/express.png" alt="" />
+                    <img src="./src/assets/logo/react.png" alt="" />
+                    <img src="./src/assets/logo/docker.png" alt="" />
+                    <img src="./src/assets/logo/mysql.png" alt="" />
+                    <img src="./src/assets/logo/github.png" alt="" />
+                    <h2>some of these are also used to make this site!</h2>
+                </div>
             </div>
         </div>
     )
