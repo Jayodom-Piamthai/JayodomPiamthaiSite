b28dc1938b8a877158a5d145be47ca71a6558916
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2024-11-28 20:15:45 +0700

    imagefix attempt2

--- src/components/screenCard/fullStack.tsx
+++ src/components/screenCard/fullStack.tsx
@@ -1,3 +1,13 @@
+import html from './src/assets/html.png'
+import css from './src/assets/css.png'
+import js from './src/assets/js.png'
+import boots from './src/assets/bootstrap.png'
+import exps from './src/assets/express.png'
+import react from './src/assets/react.png'
+import dock from './src/assets/docker.png'
+import sql from './src/assets/mysql.png'
+import github from './src/assets/github.png'
+
 function FullStack(){
     return(
         <div className="screenCard3">
@@ -11,15 +21,15 @@ function FullStack(){
                 <h1>software and tools</h1>
                 <br />
                 <div>
-                    <img src="./src/assets/html.png" alt="" />
-                    <img src="./src/assets/css.png" alt="" />
-                    <img src="./src/assets/js.png" alt="" />
-                    <img src="./src/assets/bootstrap.png" alt="" />
-                    <img src="./src/assets/express.png" alt="" />
-                    <img src="./src/assets/react.png" alt="" />
-                    <img src="./src/assets/docker.png" alt="" />
-                    <img src="./src/assets/mysql.png" alt="" />
-                    <img src="./src/assets/github.png" alt="" />
+                    <img src={html} alt="" />
+                    <img src={css} alt="" />
+                    <img src={js} alt="" />
+                    <img src={boots} alt="" />
+                    <img src={exps} alt="" />
+                    <img src={react} alt="" />
+                    <img src={react} alt="" />
+                    <img src={sql} alt="" />
+                    <img src={github} alt="" />
                     <h2>some of these are also used to make this site!</h2>
                 </div>
             </div>
