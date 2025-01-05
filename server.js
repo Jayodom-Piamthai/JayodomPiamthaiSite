477d0f985540821133bf2f30d7537b11f35567e2
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2025-01-05 13:32:45 +0700

    expressive

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+import fs from 'fs'
+import path from 'path'
+
+app.set('view engine','ejs')
+
+app.get('/' , (req,res) =>{
+    fs.readFile(path.resolve('./index.html '),'utf-8',(err,data)=>{
+        if(err){
+            return res.status(500).send('ERROR!!! sorry for the inconvenient');
+        }
+        return res.send(
+            data.replace(
+                ' <div id="root"></div>',
+                ` <div id="root">${ReactDOMServer.renderToString(<Main />)}</div>`
+            )
+        )
+    })
+})
+
+app.get('/portfolio' , (req,res) =>{
+    fs.readFile(path.resolve('./index.html '),'utf-8',(err,data)=>{
+        if(err){
+            return res.status(500).send('ERROR!!! sorry for the inconvenient');
+        }
+        return res.send(
+            data.replace(
+                ' <div id="root"></div>',
+                ` <div id="root">${ReactDOMServer.renderToString(<Portfolio />)}</div>`
+            )
+        )
+    })
+})
+
+app.use('/blog')
+
+// app.use(express.static(path.resolve(__dirname,'..','')))
+
+app.listen(1359)
