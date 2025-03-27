034b6da30819bdd561b063e5b44abe54e324e671
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2025-03-27 12:07:25 +0700

    angy

---
+++ front/src/app/component/comptest/comptest.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comptest',
+  imports: [],
+  templateUrl: "./comptest.html",
+  styles: ``
+})
+export class ComptestComponent {
+
+}
